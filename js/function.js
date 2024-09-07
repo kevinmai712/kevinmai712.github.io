5d9b63236b8d7698584c31722e327ef7e28ba09f
kevinmai712 <kevinhmai@gmail.com>
2024-09-06 20:25:21 -0500

    Under Constructions v2

---
+++ js/function.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.reveal');
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+  
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
