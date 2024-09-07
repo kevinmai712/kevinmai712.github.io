ce726a9d176eba913ad173ae9db9a02689038f33
kevinmai712 <kevinhmai@gmail.com>
2024-09-06 23:00:04 -0500

    Hero Update

--- js/function.js
+++ js/function.js
@@ -7,5 +7,21 @@ $(document).ready(function (){
             $("header, footer").removeClass('scrolled');
         }
     });
-    
+
+    VANTA.WAVES({
+        el: ".hero",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x12829b,
+        shininess: 0.00,
+        waveHeight: 5.50,
+        waveSpeed: 0.10,
+        zoom: 1
+      })
+
 });
